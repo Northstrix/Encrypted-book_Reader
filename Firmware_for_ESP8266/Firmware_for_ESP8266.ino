6055044f44cb37bc4e0cc7aa1db1fc71bf9bbc31
Maxim Bortnikov <43994704+Northstrix@users.noreply.github.com>
2022-07-18 19:07:36 +0700

    Add files via upload

---
+++ Firmware_for_ESP8266/Firmware_for_ESP8266.ino
@@ -0,0 +1,454 @@
+/*
+https://github.com/Northstrix/Encrypted-book_Reader
+Distributed under the MIT License
+© Copyright Maxim Bortnikov 2022
+For more information please visit
+https://github.com/Northstrix/Encrypted-book_Reader
+Required libraries:
+https://github.com/adafruit/Adafruit-GFX-Library
+https://github.com/adafruit/Adafruit_ILI9341
+https://github.com/adafruit/Adafruit_BusIO
+https://github.com/arduino-libraries/SD
+https://github.com/fcgdam/DES_Library
+*/
+#include <SPI.h>
+#include <SD.h>
+#include <DES.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#define TFT_CS    D2                                                         // TFT CS  pin is connected to NodeMCU pin D2
+#define TFT_RST   D3                                                         // TFT RST pin is connected to NodeMCU pin D3
+#define TFT_DC    D4                                                         // TFT DC  pin is connected to NodeMCU pin D4
+                                                                             // SCK (CLK) ---> NodeMCU pin D5 (GPIO14)
+                                                                             // MOSI(DIN) ---> NodeMCU pin D7 (GPIO13)
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+DES des;
+
+byte des_key[] = { 
+                  0x3C, 0x77, 0x6C, 0xBC, 0xFA, 0x4A, 0xD7, 0x17,
+                  0x5C, 0x82, 0x9A, 0x21, 0xF4, 0xB0, 0x93, 0xEA,
+                  0x14, 0xF7, 0x3B, 0xC8, 0x4C, 0x8A, 0x50, 0x67
+                };
+
+uint8_t Inv_S_Box[16][16] = {  
+    {0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB},  
+    {0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB},  
+    {0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E},  
+    {0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25},  
+    {0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92},  
+    {0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84},  
+    {0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06},  
+    {0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B},  
+    {0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73},  
+    {0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E},  
+    {0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B},  
+    {0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4},  
+    {0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F},  
+    {0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF},  
+    {0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61},  
+    {0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D}  
+};
+
+byte back_des_key[24];
+
+void back_k() {
+  for (int i = 0; i < 24; i++) {
+    back_des_key[i] = des_key[i];
+  }
+}
+
+void rest_k() {
+  for (int i = 0; i < 24; i++) {
+    des_key[i] = back_des_key[i];
+  }
+}
+
+int getNum(char ch)
+{
+    int num=0;
+    if(ch>='0' && ch<='9')
+    {
+        num=ch-0x30;
+    }
+    else
+    {
+        switch(ch)
+        {
+            case 'A': case 'a': num=10; break;
+            case 'B': case 'b': num=11; break;
+            case 'C': case 'c': num=12; break;
+            case 'D': case 'd': num=13; break;
+            case 'E': case 'e': num=14; break;
+            case 'F': case 'f': num=15; break;
+            default: num=0;
+        }
+    }
+    return num;
+}
+
+void incr_des_key() {
+  if (des_key[7] == 255) {
+    des_key[7] = 0;
+    if (des_key[6] == 255) {
+      des_key[6] = 0;
+      if (des_key[5] == 255) {
+        des_key[5] = 0;
+        if (des_key[4] == 255) {
+          des_key[4] = 0;
+          if (des_key[3] == 255) {
+            des_key[3] = 0;
+            if (des_key[2] == 255) {
+              des_key[2] = 0;
+              if (des_key[1] == 255) {
+                des_key[1] = 0;
+                if (des_key[0] == 255) {
+                  des_key[0] = 0;
+                } else {
+                  des_key[0]++;
+                }
+              } else {
+                des_key[1]++;
+              }
+            } else {
+              des_key[2]++;
+            }
+          } else {
+            des_key[3]++;
+          }
+        } else {
+          des_key[4]++;
+        }
+      } else {
+        des_key[5]++;
+      }
+    } else {
+      des_key[6]++;
+    }
+  } else {
+    des_key[7]++;
+  }
+
+  if (des_key[15] == 255) {
+    des_key[15] = 0;
+    if (des_key[14] == 255) {
+      des_key[14] = 0;
+      if (des_key[13] == 255) {
+        des_key[13] = 0;
+        if (des_key[12] == 255) {
+          des_key[12] = 0;
+          if (des_key[11] == 255) {
+            des_key[11] = 0;
+            if (des_key[10] == 255) {
+              des_key[10] = 0;
+              if (des_key[9] == 255) {
+                des_key[9] = 0;
+                if (des_key[8] == 255) {
+                  des_key[8] = 0;
+                } else {
+                  des_key[8]++;
+                }
+              } else {
+                des_key[9]++;
+              }
+            } else {
+              des_key[10]++;
+            }
+          } else {
+            des_key[11]++;
+          }
+        } else {
+          des_key[12]++;
+        }
+      } else {
+        des_key[13]++;
+      }
+    } else {
+      des_key[14]++;
+    }
+  } else {
+    des_key[15]++;
+  }
+
+  if (des_key[23] == 255) {
+    des_key[23] = 0;
+    if (des_key[22] == 255) {
+      des_key[22] = 0;
+      if (des_key[21] == 255) {
+        des_key[21] = 0;
+        if (des_key[20] == 255) {
+          des_key[20] = 0;
+          if (des_key[19] == 255) {
+            des_key[19] = 0;
+            if (des_key[18] == 255) {
+              des_key[18] = 0;
+              if (des_key[17] == 255) {
+                des_key[17] = 0;
+                if (des_key[16] == 255) {
+                  des_key[16] = 0;
+                } else {
+                  des_key[16]++;
+                }
+              } else {
+                des_key[17]++;
+              }
+            } else {
+              des_key[18]++;
+            }
+          } else {
+            des_key[19]++;
+          }
+        } else {
+          des_key[20]++;
+        }
+      } else {
+        des_key[21]++;
+      }
+    } else {
+      des_key[22]++;
+    }
+  } else {
+    des_key[23]++;
+  }
+}
+
+const int chipSelect = D8;
+File root;
+
+void printfiles(File dir) {
+  tft.setRotation(0);
+  tft.fillScreen(0x3186);
+  tft.setTextColor(0xce6a, 0x3186);
+  tft.setCursor(0,0);
+  tft.print("Available files:");
+  tft.setTextSize(1);
+  tft.setCursor(0,10);
+  int count = 0;
+  while (true) {
+    File entry = dir.openNextFile();
+    if (!entry) {
+      // no more files
+      break;
+    }
+    tft.print("[");
+    tft.print(count);
+    tft.print("]");
+    String nm = entry.name();
+    int i;
+    for(i = 0; i< nm.length(); i++){
+      if(i < 33)
+      tft.print(nm.charAt(i));
+    }
+    if(i > 32)
+      tft.print("...");
+    tft.println();
+    entry.close();
+    count++;
+  }
+  choose_file(count);
+}
+
+void choose_file(int mxn){
+  int nmb = 0;
+  tft.setCursor(0,304);
+  tft.setTextColor(0xffff, 0x3186);
+  for(int i = 0; i < 80; i++)
+    tft.print(" ");
+  tft.setCursor(0,304);
+  tft.print("File to be opened:0");
+  while (1){
+    if (digitalRead(16) == LOW){
+      delay(270);
+     if (digitalRead(16) == LOW){
+      tft.fillScreen(0x3186);
+      tft.setTextColor(0xce6a, 0x3186);
+      tft.setCursor(0,0);
+      break;
+      }
+    }
+    delay(90);
+    if (digitalRead(5) == LOW){
+      delay(270);
+     if (digitalRead(5) == LOW){
+      nmb++;
+      if (nmb > mxn - 1)
+        nmb = 0;
+      tft.setCursor(0,304);
+      tft.setTextColor(0xffff, 0x3186);
+      for(int i = 0; i < 80; i++)
+        tft.print(" ");
+      tft.setCursor(0,304);
+      tft.printf("File to be opened:%d", nmb);
+      }
+    }
+    delay(90);
+  }
+  root = SD.open("/");
+  root.rewindDirectory();
+  getname(root, nmb);
+  root.close();
+}
+
+void getname(File dir, int nmb) {
+  int count = 0;
+  String file_to_open;
+  while (true) {
+    File entry = dir.openNextFile();
+    if (!entry) {
+      // no more files
+      break;
+    }
+    if(count == nmb){
+      file_to_open = entry.name();
+    }
+    entry.close();
+    count++;
+  }
+  read_book(file_to_open);
+}
+
+void read_book(String fname){
+  File chosenBook;
+  back_k();
+  bool cls_b = false;
+  chosenBook = SD.open(fname, FILE_READ);
+  int n = 0;
+  while (chosenBook.available()) {
+    bool cnt = false;
+    if (n %200 == 0 && n != 0){
+      while (1){
+        if (digitalRead(16) == LOW){
+          delay(270);
+          if (digitalRead(16) == LOW){
+            tft.fillScreen(0x3186);
+            tft.setTextColor(0xce6a, 0x3186);
+            tft.setCursor(0,0);
+            break;
+          }
+        }
+        if (digitalRead(5) == LOW){
+          delay(270);
+          if (digitalRead(5) == LOW){
+            cls_b = true;
+            break;
+          }
+        }
+        delay(90);
+      }
+    }
+    if (cls_b == true)
+      break;
+    String ct;
+    for (int i = 0; i < 16; i++)
+      ct += char(chosenBook.read());
+    int ct_len = ct.length() + 1;
+    char ct_array[ct_len];
+    ct.toCharArray(ct_array, ct_len);
+    split_for_dec_des_only(ct_array, ct_len, 0);
+    n++;
+  }
+  rest_k();
+}
+
+void split_for_dec_des_only(char ct[], int ct_len, int p){
+  int br = false;
+  byte res[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (int i = 0; i < 16; i+=2){
+    if(i+p > ct_len - 1){
+      br = true;
+      break;
+    }
+    if (i == 0){
+    if(ct[i+p] != 0 && ct[i+p+1] != 0)
+    res[i] = 16*getNum(ct[i+p])+getNum(ct[i+p+1]);
+    if(ct[i+p] != 0 && ct[i+p+1] == 0)
+    res[i] = 16*getNum(ct[i+p]);
+    if(ct[i+p] == 0 && ct[i+p+1] != 0)
+    res[i] = getNum(ct[i+p+1]);
+    if(ct[i+p] == 0 && ct[i+p+1] == 0)
+    res[i] = 0;
+    }
+    else{
+    if(ct[i+p] != 0 && ct[i+p+1] != 0)
+    res[i/2] = 16*getNum(ct[i+p])+getNum(ct[i+p+1]);
+    if(ct[i+p] != 0 && ct[i+p+1] == 0)
+    res[i/2] = 16*getNum(ct[i+p]);
+    if(ct[i+p] == 0 && ct[i+p+1] != 0)
+    res[i/2] = getNum(ct[i+p+1]);
+    if(ct[i+p] == 0 && ct[i+p+1] == 0)
+    res[i/2] = 0;
+    }
+  }
+    if(br == false){
+      byte out[8];
+      des.tripleDecrypt(out, res, des_key);
+      incr_des_key();
+      Inverse_S_Box(out);
+  }
+}
+
+void Inverse_S_Box(byte decr_with_des[8]){
+  byte aft_inv_box[8];
+  for (int count; count<4; count++){
+  String strOne = "";
+  String strTwo = "";
+  int i = count * 2;
+  int j = count * 2 + 1;
+  int fir = decr_with_des[i];
+  int sec = decr_with_des[j];
+  if (fir < 16)
+  strOne += 0;
+  strOne +=  String(fir, HEX);
+  if (sec < 16)
+  strTwo += 0;
+  strTwo +=  String(sec, HEX);  
+
+  char chars_f[3];
+  char chars_s[3];
+  strOne.toCharArray(chars_f, 3);
+  strTwo.toCharArray(chars_s, 3);
+  chars_f[2] = '\0';
+  chars_s[2] = '\0';
+
+  int flc = getNum(chars_f[0]);
+  int frc = getNum(chars_f[1]);
+  int slc = getNum(chars_s[0]);
+  int src = getNum(chars_s[1]);
+
+  aft_inv_box[i] = ("%x",Inv_S_Box[flc][frc]);
+  aft_inv_box[j] = ("%x",Inv_S_Box[slc][src]);
+  }
+
+  for (int i = 0; i < 8; i++){
+    char x = aft_inv_box[i];
+    Serial.print(x);
+    tft.print(x);
+  }
+}
+
+void setup() {
+  tft.begin();
+  pinMode(16, INPUT);
+  pinMode(5, INPUT);
+  Serial.begin(115200);
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Initialising failed!");
+    tft.setRotation(0);
+    tft.fillScreen(0x3186);
+    tft.setTextColor(0xce6a, 0x3186);
+    tft.setCursor(0,0);
+    tft.print("SD card initialization failed!");
+    while(1){
+      
+    }
+  }
+}
+
+void loop() {
+  root = SD.open("/");
+  root.rewindDirectory();
+  printfiles(root);
+  root.close();
+  tft.fillScreen(0x3186);
+  tft.setCursor(0,0);
+}
---
+++ extra/3des_sbox/3des_sbox.ino
@@ -0,0 +1,218 @@
+/*
+https://github.com/Northstrix/Encrypted-book_Reader
+Distributed under the MIT License
+© Copyright Maxim Bortnikov 2022
+For more information please visit
+https://github.com/Northstrix/Encrypted-book_Reader
+Required libraries:
+https://github.com/adafruit/Adafruit-GFX-Library
+https://github.com/adafruit/Adafruit_ILI9341
+https://github.com/adafruit/Adafruit_BusIO
+https://github.com/arduino-libraries/SD
+https://github.com/fcgdam/DES_Library
+*/
+#include <DES.h>
+DES des;
+
+byte des_key[] = { 
+                  0x3C, 0x77, 0x6C, 0xBC, 0xFA, 0x4A, 0xD7, 0x17,
+                  0x5C, 0x82, 0x9A, 0x21, 0xF4, 0xB0, 0x93, 0xEA,
+                  0x14, 0xF7, 0x3B, 0xC8, 0x4C, 0x8A, 0x50, 0x67
+                };
+
+uint8_t Inv_S_Box[16][16] = {  
+    {0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB},  
+    {0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB},  
+    {0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E},  
+    {0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25},  
+    {0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92},  
+    {0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84},  
+    {0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06},  
+    {0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B},  
+    {0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73},  
+    {0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E},  
+    {0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B},  
+    {0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4},  
+    {0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F},  
+    {0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF},  
+    {0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61},  
+    {0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D}  
+};
+
+byte back_des_key[8];
+
+void back_k(){
+  for(int i = 0; i<8; i++){
+    back_des_key[i] = des_key[i];
+  }
+}
+
+void rest_k(){
+  for(int i = 0; i<8; i++){
+    des_key[i] = back_des_key[i];
+  }
+}
+
+int getNum(char ch)
+{
+    int num=0;
+    if(ch>='0' && ch<='9')
+    {
+        num=ch-0x30;
+    }
+    else
+    {
+        switch(ch)
+        {
+            case 'A': case 'a': num=10; break;
+            case 'B': case 'b': num=11; break;
+            case 'C': case 'c': num=12; break;
+            case 'D': case 'd': num=13; break;
+            case 'E': case 'e': num=14; break;
+            case 'F': case 'f': num=15; break;
+            default: num=0;
+        }
+    }
+    return num;
+}
+
+void incr_des_key() {
+  if (des_key[7] == 255) {
+    des_key[7] = 0;
+    if (des_key[6] == 255) {
+      des_key[6] = 0;
+      if (des_key[5] == 255) {
+        des_key[5] = 0;
+        if (des_key[4] == 255) {
+          des_key[4] = 0;
+          if (des_key[3] == 255) {
+            des_key[3] = 0;
+            if (des_key[2] == 255) {
+              des_key[2] = 0;
+              if (des_key[1] == 255) {
+                des_key[1] = 0;
+                if (des_key[0] == 255) {
+                  des_key[0] = 0;
+                } else {
+                  des_key[0]++;
+                }
+              } else {
+                des_key[1]++;
+              }
+            } else {
+              des_key[2]++;
+            }
+          } else {
+            des_key[3]++;
+          }
+        } else {
+          des_key[4]++;
+        }
+      } else {
+        des_key[5]++;
+      }
+    } else {
+      des_key[6]++;
+    }
+  } else {
+    des_key[7]++;
+  }
+}
+
+void split_for_dec_des_only(char ct[], int ct_len, int p){
+  int br = false;
+  byte res[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (int i = 0; i < 16; i+=2){
+    if(i+p > ct_len - 1){
+      br = true;
+      break;
+    }
+    if (i == 0){
+    if(ct[i+p] != 0 && ct[i+p+1] != 0)
+    res[i] = 16*getNum(ct[i+p])+getNum(ct[i+p+1]);
+    if(ct[i+p] != 0 && ct[i+p+1] == 0)
+    res[i] = 16*getNum(ct[i+p]);
+    if(ct[i+p] == 0 && ct[i+p+1] != 0)
+    res[i] = getNum(ct[i+p+1]);
+    if(ct[i+p] == 0 && ct[i+p+1] == 0)
+    res[i] = 0;
+    }
+    else{
+    if(ct[i+p] != 0 && ct[i+p+1] != 0)
+    res[i/2] = 16*getNum(ct[i+p])+getNum(ct[i+p+1]);
+    if(ct[i+p] != 0 && ct[i+p+1] == 0)
+    res[i/2] = 16*getNum(ct[i+p]);
+    if(ct[i+p] == 0 && ct[i+p+1] != 0)
+    res[i/2] = getNum(ct[i+p+1]);
+    if(ct[i+p] == 0 && ct[i+p+1] == 0)
+    res[i/2] = 0;
+    }
+  }
+    if(br == false){
+      byte out[8];
+      des.tripleDecrypt(out, res, des_key);
+      incr_des_key();
+      Inverse_S_Box(out);
+  }
+}
+
+void Inverse_S_Box(byte decr_with_des[8]){
+  byte aft_inv_box[8];
+  for (int count; count<4; count++){
+  String strOne = "";
+  String strTwo = "";
+  int i = count * 2;
+  int j = count * 2 + 1;
+  int fir = decr_with_des[i];
+  int sec = decr_with_des[j];
+  if (fir < 16)
+  strOne += 0;
+  strOne +=  String(fir, HEX);
+  if (sec < 16)
+  strTwo += 0;
+  strTwo +=  String(sec, HEX);  
+
+  char chars_f[3];
+  char chars_s[3];
+  strOne.toCharArray(chars_f, 3);
+  strTwo.toCharArray(chars_s, 3);
+  chars_f[2] = '\0';
+  chars_s[2] = '\0';
+
+  int flc = getNum(chars_f[0]);
+  int frc = getNum(chars_f[1]);
+  int slc = getNum(chars_s[0]);
+  int src = getNum(chars_s[1]);
+
+  aft_inv_box[i] = ("%x",Inv_S_Box[flc][frc]);
+  aft_inv_box[j] = ("%x",Inv_S_Box[slc][src]);
+  }
+
+  for (int i = 0; i < 8; i++){
+    char x = aft_inv_box[i];
+    Serial.print(x);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  //Serial.println("Hello!");
+}
+
+void loop() {
+  back_k();
+  String ct;
+  Serial.println("\nEnter ciphertext");
+  while (!Serial.available()) {}
+  ct = Serial.readString();
+  int ct_len = ct.length() + 1;
+  char ct_array[ct_len];
+  ct.toCharArray(ct_array, ct_len);
+  int ext = 0;
+  Serial.println("Plaintext");
+  while( ct_len > ext){
+    split_for_dec_des_only(ct_array, ct_len, 0+ext);
+    ext+=16;
+  }
+  rest_k();
+}
